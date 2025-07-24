9bb7c67022691122b5c4fc1653039aba0aa2ca9e
mayank18p <mayankpandeyw18@gmail.com>
2025-07-24 11:49:09 +0530

    Setting up project

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.androidcodingchallenge"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.androidcodingchallenge"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel)
+
+    // LiveData
+    implementation(libs.androidx.lifecycle.livedata)
+
+    // For lifecycle-aware components like LifecycleObserver
+    implementation(libs.androidx.lifecycle.runtime)
+
+   // Retrofit and OkHttp
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation (libs.logging.interceptor)
+
+    // Glide
+    implementation (libs.glide)
+}
